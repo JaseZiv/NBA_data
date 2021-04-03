7d7d62c33e10d56c39f133d72ff2fb4963d673c8
Jason Zivkovic <Jason.Zivkovic@reece.com.au>
2021-04-03 13:12:22 +1100

    fixed incorrect indexing

--- raw-data/nba-injuries/incremental_scraper.R
+++ raw-data/nba-injuries/incremental_scraper.R
@@ -23,6 +23,11 @@ each_link <- paste0(main_link, each_link)
 all_links <- c(first_link, each_link)
 
 page_number <- c(1, link_nodes %>% html_text())
+rem_idx <- grep("Next", page_number)
+page_number <- page_number[-rem_idx]
+
+
+all_links <- all_links[-rem_idx]
 
 # read in existing data to get the last page/url scraped
 all_injuries <- readRDS(here::here("raw-data", "nba-injuries", "nba_injuries_complete_raw.rds"))
@@ -30,26 +35,34 @@ all_injuries <- readRDS(here::here("raw-data", "nba-injuries", "nba_injuries_com
 # get the index of the last page on the site
 all_injuries_index <- all_injuries %>% pull(page_num) %>% unique() %>% as.numeric() %>% max()
 
-# then start scraping from that link
-remaining_links <- all_links[all_injuries_index:length(all_links)]
+# # then start scraping from that link
+# remaining_links <- all_links[all_injuries_index:length(all_links)]
+
 # scrape:
-for (i in 1:length(remaining_links)) {
-  print(paste("scraping page:", remaining_links[i]))
-  url <- read_html(remaining_links[i])
+for (i in all_injuries_index:length(all_links)) {
+  Sys.sleep(runif(1, 10, 18))
+  # counter
   
-  
-  each_page_df <- url %>% html_nodes(".datatable") %>% html_table() %>% data.frame()
-  
-  columnNames <- each_page_df[1,] %>% as.character()
-  
-  colnames(each_page_df) <- columnNames
-  each_page_df <- each_page_df[-1,]
-  
-  each_page_df <- each_page_df %>% mutate_all(as.character)
-  each_page_df$page_url <- all_links[i]
-  each_page_df$page_num <- page_number[i]
-  
-  all_injuries <- bind_rows(all_injuries, each_page_df)
+  if(all_injuries_index < all_injuries %>% pull(page_num) %>% unique() %>% as.numeric() %>% max()) {
+    all_injuries_index <- all_injuries_index + 1
+    
+    print(paste("scraping page:", all_links[all_injuries_index]))
+    url <- read_html(all_links[all_injuries_index])
+    
+    
+    each_page_df <- url %>% html_nodes(".datatable") %>% html_table() %>% data.frame()
+    
+    columnNames <- each_page_df[1,] %>% as.character()
+    
+    colnames(each_page_df) <- columnNames
+    each_page_df <- each_page_df[-1,]
+    
+    each_page_df <- each_page_df %>% mutate_all(as.character)
+    each_page_df$page_url <- all_links[all_injuries_index]
+    each_page_df$page_num <- page_number[all_injuries_index]
+    
+    all_injuries <- bind_rows(all_injuries, each_page_df)
+  }
   
 }
 
